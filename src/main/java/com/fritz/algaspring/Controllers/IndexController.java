abb7ac876bc76547feac7d6b2208f9bff2e91904
Fritz Henrique <fritzhenrique.dev@gmail.com>
2024-11-11 09:52:04 -0400

    feat: create put endpoint to update area

--- src/main/java/com/fritz/algaspring/Controllers/IndexController.java
+++ src/main/java/com/fritz/algaspring/Controllers/IndexController.java
@@ -4,9 +4,11 @@ import com.fritz.algaspring.Entities.Area;
 import com.fritz.algaspring.Repositories.AreaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -26,12 +28,14 @@ public class IndexController {
 
     @GetMapping("/area/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Area areaById(@PathVariable long id) {
+    public ResponseEntity<?> areaById(@PathVariable long id) {
         Area area = areaRepository.areaById(id);
         if (area == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Area ID, not exist or removed");
+            HashMap<String, String> response = new HashMap<>();
+            response.put("message", "Area ID, not exist or removed");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         }
-        return area;
+        return ResponseEntity.status(HttpStatus.OK).body(area);
     }
 
     @PostMapping("/area")
@@ -39,6 +43,28 @@ public class IndexController {
     @ResponseStatus(HttpStatus.CREATED)
     public Area saveArea(@RequestBody Area area) {
         return areaRepository.save(area);
+    }
+
+    @PutMapping("/area/{id}")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> updateArea(@PathVariable long id, @RequestBody Area area) {
+        Area area1 = areaRepository.areaById(id);
+        if (area == null) {
+            HashMap<String, String> response = new HashMap<>();
+            response.put("message", "Area ID, not exist or removed");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+
+        if (area.getName() != null) {
+            area1.setName(area.getName());
+        }
+
+        if (area.getDescription() != null) {
+            area1.setDescription(area.getDescription());
+        }
+
+        return ResponseEntity.ok().body(areaRepository.save(area1));
     }
 
     @DeleteMapping("/area/{id}")
